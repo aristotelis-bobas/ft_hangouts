f73b2ff5accd418df338bfe67f89d38b26a317e4
Aristotelis Petros Bobas <bobas@nl1lxl-110137.ttg.global>
2021-05-30 03:54:08 +0200

    clickable cardview

--- app/src/main/java/com/example/ft_hangouts/MainActivity.kt
+++ app/src/main/java/com/example/ft_hangouts/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.example.ft_hangouts.adapter.ContactAdapter
 import com.example.ft_hangouts.database.DataBaseHelper
 import com.example.ft_hangouts.databinding.ActivityMainBinding
--- app/src/main/java/com/example/ft_hangouts/adapter/ContactAdapter.kt
+++ app/src/main/java/com/example/ft_hangouts/adapter/ContactAdapter.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ft_hangouts.R
+import com.example.ft_hangouts.R.layout.contacts_list_item
 import com.example.ft_hangouts.model.ContactModel
 
 class ContactAdapter(
@@ -21,13 +23,21 @@ class ContactAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
         val adapterLayout =
             LayoutInflater.from(parent.context)
-                .inflate(R.layout.contacts_list_item, parent, false)
+                .inflate(contacts_list_item, parent, false)
         return ContactViewHolder(adapterLayout)
     }
 
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
         val contact = list[position]
         holder.textView.text = contact.name
+
+        holder.itemView.setOnClickListener {
+            Toast.makeText(context, "CARD", Toast.LENGTH_SHORT).show()
+        }
+
+        holder.textView.setOnClickListener {
+            Toast.makeText(context, "TEXT", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount(): Int {
